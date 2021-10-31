2ca28117fd7ea74007f3b7fe6b77c9a34cb3f8c4
aryan21999 <itsaryangupta1999@gmail.com>
2021-10-31 17:27:31 +0530

    making navigation bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Route } from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';  
+
+
 function App() {
-  return <div>Hi..</div>;
+  return <div>
+    <Route path='/'>
+      <AllMeetupsPage />
+    </Route>
+    <Route path='/new-meetup'>
+      <NewMeetupPage />
+    </Route>
+    <Route path='/favorites'>
+      <FavoritesPage />
+    </Route>
+  </div>
 }
 
 export default App;
